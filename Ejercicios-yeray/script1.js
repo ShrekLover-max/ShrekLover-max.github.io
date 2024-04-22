e16f01a9f465effe4eb0eb614eaa20b971e5e85b
ShrekLover-max <149491121+ShrekLover-max@users.noreply.github.com>
2024-04-22 11:57:48 +0000

    k

--- Ejercicios-yeray/script1.js
+++ Ejercicios-yeray/script1.js
@@ -2,3 +2,9 @@ alert("Este mensaje se ejecuta internamente");
 alert("Este mensaje tambien se ejucta internamente");
 alert("Este mensaje esta enfadado");
 alert("Este mensaje no le gusta salir");
+alert("Primero");
+alert("Segundo");
+alert("Es tercero?");
+alert("Perdi cuenta");
+//alert("Este mensaje no se ejecuta");
+/*No sigas este programa que no va bien, como buen ingeneniero solo me sirve que funcione y no que vaya bien*/
